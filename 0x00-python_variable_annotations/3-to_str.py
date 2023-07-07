e1639a06cf1f9f9d12c36aff5c6f35cb29689e74
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-07-08 00:23:37 +0100

    style: added newline and corrected typo error in the docummentation

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
 def to_str(n: float) -> str:
-    """Function takes in a float to return a string"""
+    """Function takes in a float parameter to return a string"""
     return str(n)
