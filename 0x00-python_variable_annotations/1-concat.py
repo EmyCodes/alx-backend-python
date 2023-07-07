05a7e944d9bd8bef084b32a8b6261d20fcd42a61
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-07-07 23:43:58 +0100

    update: assigns type annotation to the return value

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
-def concat(str1: str, str2: str):
+def concat(str1: str, str2: str) -> str:
     """This function returns the Concatenation of two string"""
     return "{}{}".format(str1, str2)
