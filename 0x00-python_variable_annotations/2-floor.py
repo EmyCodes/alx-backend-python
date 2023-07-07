e8cf8ee94793bed3bd15c02d3adc0fcfdf011ad7
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-07-08 00:10:42 +0100

    feat: makes the functon to return a whole number instead of a float

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 def floor(n: float) -> int:
     """Using VAriable Annotation,
-    Function takes in a float parameter and return a string
+    Function takes in a float parameter and return the floor of the float
     """
-    return floor(n)
+    n_str = str(n)
+    n = n_str.split(".")
+    return int(n[0])
