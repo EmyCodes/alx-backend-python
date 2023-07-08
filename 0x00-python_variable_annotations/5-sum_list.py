871dd2e3e4f9a791820f6408099a79aef2f6ee64
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-07-08 01:10:16 +0100

    feat: using variables annotation, the function takes in list but return their summation in float

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from typing import List
+
+
+"""Defining function below"""
+
+
+def sum_list(input_list: List[float]) -> float:
+    """Function takes in list as parameter to return float"""
+    sum_list = sum(input_list)
+    return sum_list
