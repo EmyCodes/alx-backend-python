88814aee78cf2549fca569ee6c7cf8a464fb5bdb
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-07-08 03:24:43 +0100

    feat: meeting alx requirements

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,4 +1,10 @@
-def safely_get_value(dct, key, default = None):
+#!?usr/bin/env python3
+from typing import Mapping, Any, Union
+from typing import TypeVar
+
+
+def safely_get_value(dct: Mapping, key: Any,
+                     default: Union[TypeVar, None]) -> Union[Any, TypeVar]:
     if key in dct:
         return dct[key]
     else:
