1503bb1e9c0963b12a0a4fcd9729e14540d0eb60
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-07-08 01:47:00 +0100

    refractor: re-imported List from typing module to handle mixed list

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-from typing import Union
+from typing import List, Union
 
 
 # def sum_mixed_list(mxd_lst: list[int | float]) -> float:
-def sum_mixed_list(mxd_lst: list[Union[int, float]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """Function takes in mixed list (int and float) as parameter to return float"""
 
     # returns the sum of the list
