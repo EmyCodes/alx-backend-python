11b6edb83018d857c0d50adb575f4f3936c41499
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-07-08 01:33:33 +0100

    feat: returns the sum of the list

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+from typing import List
+
+
+def sum_mixed_list(mxd_lst: List[int]) -> float:
+    """Function takes in mixed list (int and float) as parameter to return float"""
+
+    # returns the sum of the list
+    sum_list = sum(mxd_lst)
+    return sum_list
