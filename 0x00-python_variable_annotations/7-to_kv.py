d796754889d7512bdb242e0285801c0ef9035ca2
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-07-08 02:14:12 +0100

    style: passes PYCODESTYLE

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -4,7 +4,7 @@ from typing import Tuple, Union
 
 # def sum_mixed_list(mxd_lst: list[int | float]) -> float:
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
-    """Function takes in mixed list (int and float)
-    as parameter to return float"""
-    
+    """Type-annotated function  that takes a string k and an int OR 
+    float v as arguments and returns a tuple."""
+
     return (k, pow(v, 2))
