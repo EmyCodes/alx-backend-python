3d424ea2c6e214ed273c3a32f172fcc4fc3d541a
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-07-08 03:30:17 +0100

    refractor: misspelt shebang corrected and import line reduced

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,7 +1,5 @@
-#!usr/bin/env python3
-from typing import Mapping, Any, Union
-from typing import TypeVar
-
+#!/usr/bin/env python3
+from typing import Mapping, Any, Union, TypeVar
 
 def safely_get_value(dct: Mapping, key: Any,
                      default: Union[TypeVar, None]) -> Union[Any, TypeVar]:
