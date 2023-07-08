3dfe867d0cdf2c128ae498ecb305561c03a2ebe8
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-07-08 03:26:26 +0100

    docs: Added documentation

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -5,6 +5,7 @@ from typing import TypeVar
 
 def safely_get_value(dct: Mapping, key: Any,
                      default: Union[TypeVar, None]) -> Union[Any, TypeVar]:
+    """Worked on thi codebase"""
     if key in dct:
         return dct[key]
     else:
