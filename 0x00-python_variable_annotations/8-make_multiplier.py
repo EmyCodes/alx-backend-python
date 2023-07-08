685fb092ebbbaf741a89fbc2a3b5d7ea0c372874
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-07-08 02:32:16 +0100

    feat: returns function that multiplies a float

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """Returns Function that multiplies a float"""
+    def multiply_(value: float) -> float:
+        """"Inner function multiplies outer"""
+        return value * multiplier
+    
+    return multiply_
