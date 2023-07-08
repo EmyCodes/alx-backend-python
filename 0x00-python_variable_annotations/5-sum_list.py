3f656de26f80876620254e7401a28ef64b2be4b4
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-07-08 01:26:47 +0100

    style: added newline

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -5,5 +5,6 @@ from typing import List
 def sum_list(input_list: List[float]) -> float:
     """Function takes in list as parameter to return float"""
     sum_list = sum(input_list)
+
     # returns the sum of the list
     return sum_list
