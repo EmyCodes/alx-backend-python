5aaa39846f05a03088c6bcec72038817cb05133e
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-07-08 01:42:31 +0100

    feat: imported Union from typing module to handle mixed list

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-from typing import List
+from typing import Union
 
 
-def sum_mixed_list(mxd_lst: List[int]) -> float:
+# def sum_mixed_list(mxd_lst: list[int | float]) -> float:
+def sum_mixed_list(mxd_lst: list[Union[int, float]]) -> float:
     """Function takes in mixed list (int and float) as parameter to return float"""
 
     # returns the sum of the list
