ad7af4419ee30c0a75b4bfde65366f026d8978a3
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-07-10 15:04:50 +0100

    feat: writes some coroutines

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import asyncio
+from random import uniform
+
+
+async def wait_random(max_delay=10):
+    """an asynchronous coroutine that takes in an integer argument
+    (max_delay, with a default value of 10) named wait_random that waits
+    for a random delay between 0 and max_delay
+    (included and float value) seconds and eventually returns it."""
+    delay = uniform(0, max_delay)
+    await asyncio.sleep(delay)
+
+async def main():
+    """Passing the coroutine here"""
+    await asyncio.gather(
+        wait_random(),
+        wait_random(5),
+        wait_random(15)
+    )
+
+if __name__ == "__main__":
+    asyncio.run(main())
